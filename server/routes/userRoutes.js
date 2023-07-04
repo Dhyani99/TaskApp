b0e50b4fb271dadf56d1ddfc0558f07533e5b088
Dhyani <gandhidhyani@gmail.com>
2023-07-03 22:20:55 -0400

    Updated login and register api

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -11,6 +11,25 @@ router.get("/", function (req, res, next) {
     .catch(next);
 });
 
+router.post("/login", async function (req, res, next) {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      const result = req.body.password === user.password;
+      console.log(user.password);
+      if (result) {
+        res.status(200).send();
+      } else {
+        res.status(400).json({ error: "password doesn't match" });
+      }
+    } else {
+      res.status(400).json({ error: "User doesn't exist" });
+    }
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+});
+
 router.get("/:id", function (req, res, next) {
   User.findOne({ _id: req.params.id })
     .then(function (user) {
@@ -19,13 +38,14 @@ router.get("/:id", function (req, res, next) {
     .catch(next);
 });
 
-// add a new user to database
-router.post("/", function (req, res, next) {
-  User.create(req.body)
-    .then(function (user) {
-      res.send(user);
-    })
-    .catch(next);
+router.post("/register", async (req, res) => {
+  const user = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+  return res.status(200).json(user);
 });
 
 // update a user in the database
